e4d9a2d6bdcf7c2e5c57d2945019bccf79d2365a
Xin Lyu <lvxinkelvin@gmail.com>
2020-12-16 20:20:05 -0500

    Add Collision of bullets and cubes

--- Assets/_Scripts/BulletBehaviour.cs
+++ Assets/_Scripts/BulletBehaviour.cs
@@ -5,20 +5,43 @@ using UnityEngine;
 [System.Serializable]
 public class BulletBehaviour : MonoBehaviour
 {
+    public Vector3 size;
+    public Vector3 max;
+    public Vector3 min;
+    public float radius;
+    public bool isColliding;
+    public Vector3 position;
+
+    public List<CubeBehaviour> contacts;
+
     public float speed;
     public Vector3 direction;
     public float range;
 
+    private MeshFilter meshFilter;
+    private Bounds bounds;
+
     // Start is called before the first frame update
     void Start()
     {
+        meshFilter = GetComponent<MeshFilter>();
+
+        bounds = meshFilter.mesh.bounds;
+        size = bounds.size;
+        //max = Vector3.Scale(bounds.max, transform.localScale) + transform.position;
+        //min = Vector3.Scale(bounds.min, transform.localScale) + transform.position;
+        radius = (max.x - min.x) / 2.0f;
+        position = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        position = transform.position;
         _Move();
         _CheckBounds();
+        max = Vector3.Scale(bounds.max, transform.localScale) + transform.position;
+        min = Vector3.Scale(bounds.min, transform.localScale) + transform.position;
     }
 
     private void _Move()
--- Assets/_Scripts/CollisionManager.cs
+++ Assets/_Scripts/CollisionManager.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class CollisionManager : MonoBehaviour
 {
     public CubeBehaviour[] actors;
+    public BulletBehaviour[] bullets;
 
     // Start is called before the first frame update
     void Start()
     {
         actors = FindObjectsOfType<CubeBehaviour>();
+        bullets = FindObjectsOfType<BulletBehaviour>();
     }
 
     // Update is called once per frame
@@ -26,8 +28,14 @@ public class CollisionManager : MonoBehaviour
                 }
             }
         }
+        for (int i = 0; i < actors.Length; i++)
+        {
+            for (int j = 0; j < bullets.Length; j++)
+            {
+                CheckSphereAABB(bullets[j], actors[i]);
+            }
+        }
     }
-
     public static void CheckAABBs(CubeBehaviour a, CubeBehaviour b)
     {
         if ((a.min.x <= b.max.x && a.max.x >= b.min.x) &&
@@ -42,7 +50,7 @@ public class CollisionManager : MonoBehaviour
                 {
                     a.onTheGround = true;
                 }
-                else if(b.onTheGround)
+                else if (b.onTheGround)
                 {
                     a.onTheGround = true;
                 }
@@ -54,7 +62,7 @@ public class CollisionManager : MonoBehaviour
             {
                 a.contacts.Remove(b);
                 a.isColliding = false;
-                if(b.isGround)
+                if (b.isGround)
                 {
                     a.onTheGround = false;
                 }
@@ -63,7 +71,37 @@ public class CollisionManager : MonoBehaviour
                     a.onTheGround = false;
                 }
             }
-           
+
         }
+    }
+    public static void CheckSphereAABB(BulletBehaviour a, CubeBehaviour b)
+    {
+        float ax = a.transform.position.x;
+        float ay = a.transform.position.y;
+        float az = a.transform.position.z;
+        float x = Mathf.Max(b.min.x, Mathf.Min(ax, b.max.x));
+        float y = Mathf.Max(b.min.y, Mathf.Min(ay, b.max.y));
+        float z = Mathf.Max(b.min.z, Mathf.Min(az, b.max.z));
+        float distance = Mathf.Sqrt((x - ax) * (x - ax) +
+                                  (y - ay) * (y - ay) +
+                                  (z - az) * (z - az));
+        if (distance < a.radius+0.5f)
+        {
+            if (!a.contacts.Contains(b))
+            {
+                a.contacts.Add(b);
+                a.isColliding = true;
+            }
+        }
+        else
+        {
+            if (a.contacts.Contains(b))
+            {
+                a.contacts.Remove(b);
+                a.isColliding = false;
+            }
+
+        }
+        
     }
 }
